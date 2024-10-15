b26b8c38633bd121471dab482b84e0bf4156a9e8
EconLQ <zavliybogdan@gmail.com>
2024-10-15 17:11:29 +0300

    changed number of block to 100

--- src/main/java/com/liquiduspro/util/Constants.java
+++ src/main/java/com/liquiduspro/util/Constants.java
@@ -3,7 +3,7 @@ package com.liquiduspro.util;
 public final class Constants {
     public static final int DIFFICULTY = 5;
     public static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
-    public static final int NUM_OF_BLOCKS = 5;
+    public static final int NUM_OF_BLOCKS = 100;
 
     private Constants() {
     }
