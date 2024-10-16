8925e48e7cec4e654476084a39b61ec8dcca459f
EconLQ <zavliybogdan@gmail.com>
2024-10-16 21:05:56 +0300

    updated constants

--- src/main/java/com/liquiduspro/util/Constants.java
+++ src/main/java/com/liquiduspro/util/Constants.java
@@ -3,7 +3,8 @@ package com.liquiduspro.util;
 public final class Constants {
     public static final int DIFFICULTY = 5;
     public static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
-    public static final int NUM_OF_BLOCKS = 100;
+    public static final int NUM_OF_BLOCKS = 10;
+    public static final float INITIAL_AMOUNT_OF_COINS = 100f;
 
     private Constants() {
     }
