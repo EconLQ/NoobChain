0e22c59cda4047e0386134331114c528d43db877
EconLQ <zavliybogdan@gmail.com>
2024-10-16 21:07:23 +0300

    added methods for applying and verifying ECDSA signatures, getting encoded string from key, getting merkle root

--- src/main/java/com/liquiduspro/util/StringUtil.java
+++ src/main/java/com/liquiduspro/util/StringUtil.java
@@ -1,12 +1,16 @@
 package com.liquiduspro.util;
 
 
+import com.liquiduspro.domain.transaction.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public final class StringUtil {
@@ -27,5 +31,62 @@ public final class StringUtil {
             logger.warn("Error while hashing string: {}", e.toString());
             throw new RuntimeException(e);
         }
+    }
+
+    // apply ECDSA signature and return the result in bytes
+    public static byte[] applyECDSASignature(final PrivateKey privateKey, final String input) {
+        Signature ecdsa;
+        byte[] output;
+        try {
+            ecdsa = Signature.getInstance("ECDSA", "BC");
+            ecdsa.initSign(privateKey);
+            byte[] strByte = input.getBytes();
+            ecdsa.update(strByte);
+            output = ecdsa.sign();  // real signature
+        } catch (NoSuchAlgorithmException | SignatureException | NoSuchProviderException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+    // verify ECDSA signature
+    public static boolean verifyECDSASignature(final PublicKey publicKey, final String data, final byte[] signature) {
+        try {
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidKeyException | SignatureException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getStringFromKey(final Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public static String getMerkleRoot(final List<Transaction> transactions) {
+        int count = transactions.size();
+        List<String> previousTreeLayer = transactions
+                .stream()
+                .map(Transaction::getTransactionId)
+                .collect(Collectors.toList());
+
+        List<String> treeLayer = new ArrayList<>();
+        while (count > 1) {
+            if (count % 2 != 0) {
+                treeLayer.add(previousTreeLayer.get(count - 1));
+                count--;
+            }
+            for (int i = 0; i < count; i += 2) {
+                String parent = applySha256(previousTreeLayer.get(i) + previousTreeLayer.get(i + 1));
+                treeLayer.add(parent);
+            }
+            previousTreeLayer = treeLayer;
+            treeLayer = new ArrayList<>();
+            count = previousTreeLayer.size();
+        }
+        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+        return merkleRoot;
     }
 }
