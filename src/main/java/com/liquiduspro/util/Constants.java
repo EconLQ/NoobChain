9eb80175328c2bb8d8d438c8b7f30ed0a441a064
EconLQ <zavliybogdan@gmail.com>
2024-10-18 13:52:16 +0300

    added number of transactions const

--- src/main/java/com/liquiduspro/util/Constants.java
+++ src/main/java/com/liquiduspro/util/Constants.java
@@ -4,6 +4,7 @@ public final class Constants {
     public static final int DIFFICULTY = 5;
     public static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
     public static final int NUM_OF_BLOCKS = 10;
+    public static final int NUM_OF_TRANSACTIONS = 20;
     public static final float INITIAL_AMOUNT_OF_COINS = 100f;
 
     private Constants() {
